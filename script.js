5a3529b107eabc20e1278f45c2b6dd4eba77d295
Vectorax <rice.jacob@gmail.com>
2022-04-18 22:46:47 -0500

    starting js

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(a, b){
+    return a + b
+}
+
+function subtract(a, b){
+    return a - b
+}
+
+function multiply(a, b){
+    return a * b
+}
+
+function divide(a, b){
+    return a / b
+}
+
+function stepOne(a){
+    let a = a;
+}
+
+function input(){
+    let inputArea = document.getElementById("solutionArea");
+    inputArea.innerText()
+}
