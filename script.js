cc8209af9af21169fe448979c0ab4e30504bb10a
vectorax <99517621+vectorax@users.noreply.github.com>
2022-04-19 17:11:46 -0500

    savepoint

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let displayValue ='';
 let storedValue ='';
+let operator = '';
+let solution = '';
 
 const keypad = document.querySelector('#buttonContainer');
+const display = document.getElementById('display');
+const subDisplay = document.getElementById('subDisplay');
+
 keypad.addEventListener('click',fillDisplay,false);
 
-function add(a, b){
+ function add(a, b){
     return a + b
-}
+ }
 
 function subtract(a, b){
     return a - b
@@ -18,7 +23,7 @@ function multiply(a, b){
 
 function divide(a, b){
     return a / b
-}
+} 
 
 
 
@@ -28,8 +33,8 @@ function input(){
 }
 
 function fillDisplay(e){
-    const display = document.getElementById('solutionBox');
-    if(e.target !== e.currentTarget){
+    
+    if(e.target !== e.currentTarget && e.target.className == 'numberButton'){
         let clickedButton = e.target.value;
         displayValue = displayValue.concat(clickedButton);
         display.innerText = displayValue;
@@ -39,6 +44,25 @@ function fillDisplay(e){
 function storeOne(e){
     storedValue = displayValue;
     displayValue = '';
-    console.log(e.target);
+    operator = e.target.value;
+    display.innerText = displayValue;
+    subDisplay.innerText = storedValue;
+}
 
+function operate(){
+    let a = storedValue;
+    let b = displayValue;
+    console.log(storedValue);
+    console.log(b);
+    if(operator == add){
+        solution = add(a,b)
+    }
+    else if(operator == subtract){
+        solution = subtract(a,b)
+    }
+    else if(operator == multiply){
+        solution = multiply(a,b)
+    }
+    else {solution = divide(a,b)}
+    display.innerText = solution;
 }
